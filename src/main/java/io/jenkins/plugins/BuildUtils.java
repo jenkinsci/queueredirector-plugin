a11fde91d26835c4cfcb3abd2a9fed8d479b49a6
papa <papa>
2024-10-14 14:34:38 +0900

    cleanup

--- src/main/java/io/jenkins/plugins/BuildUtils.java
+++ src/main/java/io/jenkins/plugins/BuildUtils.java
@@ -3,56 +3,44 @@ package io.jenkins.plugins;
 import hudson.model.Job;
 import hudson.model.Queue;
 import hudson.model.Run;
-import java.util.Collection;
 import java.util.Optional;
 import jenkins.model.Jenkins;
 
 public class BuildUtils {
+
     public static boolean buildStarted(Long queueId) {
         if (Jenkins.getInstanceOrNull() == null) {
-            // Jenkins is not running
             return false;
         }
-        // Get the queue item
-        Queue.Item item = Jenkins.get().getQueue().getItem(queueId);
+
+        // Check if the item is in the queue
+        Queue queue = Jenkins.get().getQueue();
+        Queue.Item item = queue.getItem(queueId);
         if (item == null) {
             // If the item is not in the queue, it might have started
             return true;
         }
 
-        // Maybe the job was just started and is now in the LeftItem queue
-        Collection<Queue.LeftItem> leftItems = Jenkins.get().getQueue().getLeftItems();
-        for (Queue.LeftItem leftitem : leftItems) {
-            if (leftitem.getId() == queueId) {
-                return true;
-            }
-        }
-
-        return false;
+        // Check if the job was started and is now in the LeftItem queue
+        return queue.getLeftItems().stream().anyMatch(leftItem -> leftItem.getId() == queueId);
     }
 
     public static Optional<String> buildUrl(Job<?, ?> job, Long queueId) {
-        if (Jenkins.getInstanceOrNull() == null) {
-            // Jenkins is not running
+        if (Jenkins.getInstanceOrNull() == null || job == null) {
             return Optional.empty();
         }
 
-        for (Run<?, ?> run : job.getBuilds()) {
-            if (run.getQueueId() == queueId) {
-                // Construct the URL for the build
-                return Optional.of(Jenkins.get().getRootUrl() + run.getUrl());
-            }
-        }
-
-        // If no matching build is found, return null or an appropriate message
-        return Optional.empty();
+        return job.getBuilds().stream()
+                .filter(run -> run.getQueueId() == queueId)
+                .findFirst()
+                .map(run -> Jenkins.get().getRootUrl() + run.getUrl());
     }
 
     public static Optional<String> buildUrl(Long queueId) {
         if (Jenkins.getInstanceOrNull() == null) {
-            // Jenkins is not running
             return Optional.empty();
         }
+
         for (Job<?, ?> job : Jenkins.get().getAllItems(Job.class)) {
             for (Run<?, ?> run : job.getBuilds()) {
                 if (run.getQueueId() == queueId) {
